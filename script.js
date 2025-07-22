d0a127fcc0b3876e5ab8ad43e162e16d65d3187a
Tomek <tomek.wojak796@gmail.com>
2025-07-22 13:57:35 +0200

    practicing higher-order functions

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const osoba = {
-	imie: "Kuba",
-	wiek: 25,
-	hobby: ["programowanie", "muzyka", "bieganie"],
-};
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const cubes = numbers.map((el) => el * el * el);
 
-const name = osoba.imie
-const wiek = osoba.wiek
+const test = [];
 
-const test = [...osoba.hobby]
-test.push('czytanie')
-console.log(name, wiek, test);
+for (number of numbers) {
+	test.push(number * number * number);
+}
+
+let storkList = [
+	{ name: "ser", price: 3 },
+	{ name: "chleb", price: 3 },
+	{ name: "masło", price: 2 },
+];
+
+const test2 = storkList.map(el => el.price)
+console.log(test2);
