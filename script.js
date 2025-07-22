efb8afe58bbc89809025c7a2957565d498e43048
Tomek <tomek.wojak796@gmail.com>
2025-07-22 14:08:25 +0200

    practicing making own custom functions

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const cubes = numbers.map((el) => el * el * el);
-
-const test = [];
-
-for (number of numbers) {
-	test.push(number * number * number);
+function doubler(callback, ...value) {
+	callback(value);
 }
 
-let storkList = [
-	{ name: "ser", price: 3 },
-	{ name: "chleb", price: 3 },
-	{ name: "masło", price: 2 },
-];
+const test = (x) => {
+	x.forEach((element) => {
+		console.log(element);
+	});
+};
 
-const test2 = storkList.map(el => el.price)
-console.log(test2);
+doubler(test, 1, 2, 3)
