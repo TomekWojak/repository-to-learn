85994d94719250263e65ce8844cc15b93197034b
Tomek <tomek.wojak796@gmail.com>
2025-07-26 15:32:27 +0200

    attacking player mini game

--- script.js
+++ script.js
@@ -11,12 +11,29 @@ class Actor {
 	distanceTo(otherPlayer) {
 		let dx = otherPlayer.x - this.x;
 		let dy = otherPlayer.y - this.y;
+		return Math.hypot(dx, dy);
 	}
 }
 
-class PLayer extends Actor {
+class Player extends Actor {
 	constructor(startX, startY) {
 		super(startX, startY);
-		this.hp = 100
+		this.hp = 100;
 	}
 }
+
+class Enemy extends Actor {
+	attack(player) {
+		if (this.distanceTo(player) < 4) {
+			player.hp -= 10;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+const player = new Player(3, 4);
+const enemy = new Enemy(1, 2);
+
+
