aa45ef9172523991da56e286c15494dce53d1114
Tomek <tomek.wojak796@gmail.com>
2025-07-30 15:38:25 +0200

    created function which add transactions

--- Js-projects/Finance-manager/script.js
+++ Js-projects/Finance-manager/script.js
@@ -39,11 +39,56 @@ const checkForm = () => {
 		amountInput.value !== "" &&
 		categorySelect.value !== "none"
 	) {
-		console.log("ok");
+		createNewTransaction();
 	} else {
 		alert("Wypełnij wszystkie pola");
 	}
 };
+
+const createNewTransaction = () => {
+	const newTransaction = document.createElement("div");
+	newTransaction.classList.add("transactions__transaction");
+	newTransaction.setAttribute("id", ID);
+	selectCategory();
+	checkCategory(selectedCategory);
+	newTransaction.innerHTML = `<p class="transactions__transaction-name">${categoryIcon} ${nameInput.value}</p>
+    <p class="transactions__transaction-amount">${amountInput.value}zł <button
+    class="transactions__transaction-delete" aria-label="Usuń transakcję z listy"><i
+    class="fas fa-times"></i></button></p>`;
+
+	amountInput.value > 0
+		? incomeSection.append(newTransaction) &&
+		  newTransaction.classList.add("income")
+		: expensesSection.append(newTransaction) &&
+		  newTransaction.classList.add("expenses");
+
+	moneyArr.push(parseFloat(amountInput.value));
+
+	closePanel();
+	ID++;
+};
+
+const checkCategory = (transaction) => {
+	switch (transaction) {
+		case "[ + ] Przychód":
+			categoryIcon = '<i class="fas fa-money-bill-wave"></i>';
+			break;
+		case "[ - ] Zakupy":
+			categoryIcon = '<i class="fas fa-cart-arrow-down"></i>';
+			break;
+		case "[ - ] Jedzenie":
+			categoryIcon = '<i class="fas fa-hamburger"></i>';
+			break;
+		case "[ - ] Kino":
+			categoryIcon = '<i class="fas fa-film"></i>';
+			break;
+	}
+};
+
+const selectCategory = () => {
+	selectedCategory = categorySelect.options[categorySelect.selectedIndex].text;
+};
+
 saveBtn.addEventListener("click", checkForm);
 addTransactionBtn.addEventListener("click", showPanel);
 cancelBtn.addEventListener("click", closePanel);
