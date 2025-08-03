6f119c670fa4def71b322d4e7c44cd425d2c76e5
Tomek <tomek.wojak796@gmail.com>
2025-08-03 16:50:56 +0200

    downloaded all elements to js from html

---
+++ Js-projects/02Time-counter/script.js
@@ -0,0 +1,22 @@
+const settings = document.querySelector(".counter-app__settings");
+const settingsBtn = document.querySelector(".counter-app__settings-btn");
+const imageSection = document.querySelector(".counter-app__image-section");
+
+const eventName = document.querySelector("#event-name");
+const eventDay = document.querySelector("#event-day");
+const eventMonth = document.querySelector("#event-month");
+const eventYear = document.querySelector("#event-year");
+const eventImg = document.querySelector("#event-image");
+
+const daysCount = document.querySelector(".counter-app__number--days");
+const hoursCount = document.querySelector(".counter-app__number--hours");
+const minutesCount = document.querySelector(".counter-app__number--minutes");
+const secondsCount = document.querySelector(".counter-app__number--seconds");
+
+const saveBtn = document.querySelector(".counter-app__save");
+const eventSpan = document.querySelector(".event");
+let usersTime;
+
+settingsBtn.addEventListener("click", () => {
+	settings.classList.toggle("active");
+});
