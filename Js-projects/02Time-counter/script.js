0a2a8980fe7684f26efab383ff9494c3f79fbd79
Tomek <tomek.wojak796@gmail.com>
2025-08-03 18:11:23 +0200

    added function which gets users date

--- Js-projects/02Time-counter/script.js
+++ Js-projects/02Time-counter/script.js
@@ -17,6 +17,16 @@ const saveBtn = document.querySelector(".counter-app__save");
 const eventSpan = document.querySelector(".event");
 let usersTime;
 
+const appUpdate = () => {
+	eventSpan.textContent = eventName.value;
+	imageSection.style.backgroundImage = `url('${eventImg.value}')`;
+	usersTime = new Date(
+		`${eventMonth.value} ${eventDay.value} ${eventYear.value}`
+	);
+	console.log(usersTime);
+};
+
 settingsBtn.addEventListener("click", () => {
 	settings.classList.toggle("active");
 });
+saveBtn.addEventListener("click", appUpdate);
