eca118f62597fc4251519474f0ab61ed7baf8800
Tomek <tomek.wojak796@gmail.com>
2025-08-04 18:01:37 +0200

    practicing keyboards events

--- book-exercises/script.js
+++ book-exercises/script.js
@@ -65,19 +65,36 @@ document.title = "Nowa nazwa strony";
 
 const root = document.documentElement;
 
-let hasCreatedBox = false;
+// let hasCreatedBox = false;
 
-document.body.addEventListener("mousemove", (e) => {
-	if (!hasCreatedBox) {
-		const box = document.createElement("div");
-		box.classList.add("box");
-		document.body.append(box);
-		hasCreatedBox = true;
+// document.body.addEventListener("mousemove", (e) => {
+// 	if (!hasCreatedBox) {
+// 		const box = document.createElement("div");
+// 		box.classList.add("box");
+// 		document.body.append(box);
+// 		hasCreatedBox = true;
+// 	}
+
+// 	const positionX = e.clientX + 'px';
+// 	const positionY = e.clientY + 'px';
+
+// 	root.style.setProperty('--position-x', positionX)
+// 	root.style.setProperty('--position-y', positionY)
+// });
+
+let posX = 0;
+let posY = 0;
+document.body.addEventListener("keydown", (e) => {
+	if (e.key === "w") {
+		posY -= 5;
+	} else if (e.key === "a") {
+		posX -= 5;
+	} else if (e.key === "s") {
+		posY += 5;
+	} else if (e.key === "d") {
+		posX += 5;
 	}
-
-	const positionX = e.clientX + 'px';
-	const positionY = e.clientY + 'px';
-
-	root.style.setProperty('--position-x', positionX)
-	root.style.setProperty('--position-y', positionY)
+	console.log(e);
+	root.style.setProperty("--posY", posY + "px");
+	root.style.setProperty("--posX", posX + "px");
 });
