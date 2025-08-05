7a60ce6142196368bcf5b38bccbd529b34100136
Tomek <tomek.wojak796@gmail.com>
2025-08-06 01:11:34 +0200

    learning gulp

---
+++ learning-js-course/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+
+// gulp.task("test", function (done) {
+// 	console.log("test");
+// 	done();
+// });
+// Stara metoda
+
+
+
+function test2(done) {
+	console.log("test2");
+	done();
+}
+
+exports.test2 = test2;
