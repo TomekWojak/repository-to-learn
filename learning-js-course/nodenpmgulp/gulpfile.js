b3fb929575a83336dc3fcff46d127e956329b5b7
Tomek <tomek.wojak796@gmail.com>
2025-08-06 15:28:11 +0200

    added images minify

--- learning-js-course/nodenpmgulp/gulpfile.js
+++ learning-js-course/nodenpmgulp/gulpfile.js
@@ -5,19 +5,29 @@ const autoprefixer = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+
+const paths = {
+	sass: "./src/sass/**/*.scss",
+	js: "./src/js/**/*.js",
+	img: "./src/img/*",
+	sassDest: "./dist/css",
+	jsDest: "./dist/js",
+	imgDest: "./dist/img",
+};
 
 function sassCompiler(done) {
-	src("./src/sass/**/*.scss") //** oznaczają że ma przeszukać folder sass, jeśli będą jakieś podfoldery to je również przeszukaj, * - przeszuka wszystkie pliki które mają rozszerzenie .scss */
+	src(paths.sass) //** oznaczają że ma przeszukać folder sass, jeśli będą jakieś podfoldery to je również przeszukaj, * - przeszuka wszystkie pliki które mają rozszerzenie .scss */
 		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer())
 		.pipe(cssnano())
 		.pipe(rename({ suffix: ".min" }))
-		.pipe(dest("./dist/css"));
+		.pipe(dest(paths.sassDest));
 	done();
 }
 
 function javaScript(done) {
-	src("./src/js/**/*.js")
+	src(paths.js)
 		.pipe(
 			babel({
 				presets: ["@babel/env"],
@@ -25,8 +35,14 @@ function javaScript(done) {
 		)
 		.pipe(uglify())
 		.pipe(rename({ suffix: ".min" }))
-		.pipe(dest("./dist/js"));
+		.pipe(dest(paths.jsDest));
+	done();
+}
+function convertImages(done) {
+	src(paths.img, { encoding: false })
+		.pipe(imagemin())
+		.pipe(dest(paths.imgDest));
 	done();
 }
 
-exports.default = series(sassCompiler, javaScript);
+exports.default = series(sassCompiler, javaScript, convertImages);
