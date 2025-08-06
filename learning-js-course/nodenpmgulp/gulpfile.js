3bc134505d3274f4bb39aa87bc01905e7865f1e0
Tomek <tomek.wojak796@gmail.com>
2025-08-06 16:39:40 +0200

    added browser sync

--- learning-js-course/nodenpmgulp/gulpfile.js
+++ learning-js-course/nodenpmgulp/gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, parallel, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
 const autoprefixer = require("gulp-autoprefixer");
@@ -6,7 +6,9 @@ const rename = require("gulp-rename");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const imagemin = require("gulp-imagemin");
-
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload;
 const paths = {
 	sass: "./src/sass/**/*.scss",
 	js: "./src/js/**/*.js",
@@ -18,16 +20,19 @@ const paths = {
 
 function sassCompiler(done) {
 	src(paths.sass) //** oznaczają że ma przeszukać folder sass, jeśli będą jakieś podfoldery to je również przeszukaj, * - przeszuka wszystkie pliki które mają rozszerzenie .scss */
+		.pipe(sourcemaps.init())
 		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer())
 		.pipe(cssnano())
 		.pipe(rename({ suffix: ".min" }))
+		.pipe(sourcemaps.write())
 		.pipe(dest(paths.sassDest));
 	done();
 }
 
 function javaScript(done) {
 	src(paths.js)
+		.pipe(sourcemaps.init())
 		.pipe(
 			babel({
 				presets: ["@babel/env"],
@@ -35,6 +40,7 @@ function javaScript(done) {
 		)
 		.pipe(uglify())
 		.pipe(rename({ suffix: ".min" }))
+		.pipe(sourcemaps.write())
 		.pipe(dest(paths.jsDest));
 	done();
 }
@@ -45,4 +51,21 @@ function convertImages(done) {
 	done();
 }
 
-exports.default = series(sassCompiler, javaScript, convertImages);
+function startBrowserSync(done) {
+	browserSync.init({
+		server: {
+			baseDir: "./",
+		},
+	});
+	done();
+}
+
+function watchForChanges(done) {
+	watch("./*html").on("change", reload); //Obserwuj wszystkie pliki z rozszerzeniem .html
+
+	done();
+}
+
+const mainFunctions = parallel(sassCompiler, javaScript, convertImages);
+
+exports.default = series(mainFunctions, startBrowserSync, watchForChanges);
