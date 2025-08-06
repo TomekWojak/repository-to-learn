0d2c5dfec6522144e5c0bc7c2958034cfb268711
Tomek <tomek.wojak796@gmail.com>
2025-08-06 13:34:11 +0200

    added cssnano, autoprefixer, gulp-rename

--- learning-js-course/nodenpmgulp/gulpfile.js
+++ learning-js-course/nodenpmgulp/gulpfile.js
@@ -1,11 +1,17 @@
 const { src, dest } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const cssnano = require("gulp-cssnano");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
 
 function sassCompiler(done) {
 	src("./src/sass/**/*.scss") //** oznaczają że ma przeszukać folder sass, jeśli będą jakieś podfoldery to je również przeszukaj, * - przeszuka wszystkie pliki które mają rozszerzenie .scss */
 		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cssnano())
+		.pipe(rename({suffix: '.min'}))
 		.pipe(dest("./dist/css"));
 	done();
 }
 
-exports.default = sassCompiler
+exports.default = sassCompiler;
