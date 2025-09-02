9dc4033a878c570f9e0389d8c1d93cafc09b69e7
Tomek <tomek.wojak796@gmail.com>
2025-09-02 21:36:20 +0200

    learned ways to create arrays in ts

--- Learning-typescript/script.ts
+++ Learning-typescript/script.ts
@@ -1,3 +1,10 @@
 const age: number = 54;
 let surname: string = "Potter";
 const isAdult: boolean = true;
+
+
+const arr: number[] = [1, 2, 3, 4];
+
+const arr2: Array<number> = [1, 2, 3];
+
+const arr3 = Array(1, 2, 3, 4);
