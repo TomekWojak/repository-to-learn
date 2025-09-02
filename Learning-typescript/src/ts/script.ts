853d0d1fe5744de7814c707032e83e818f10b26d
Tomek <tomek.wojak796@gmail.com>
2025-09-02 22:22:44 +0200

    learned Enumies

--- Learning-typescript/src/ts/script.ts
+++ Learning-typescript/src/ts/script.ts
@@ -2,7 +2,6 @@ const age: number = 54;
 let surname: string = "Potter";
 const isAdult: boolean = true;
 
-
 const arr: number[] = [1, 2, 3, 4];
 
 const arr2: Array<number> = [1, 2, 3];
@@ -12,6 +11,17 @@ const arr3 = Array(1, 2, 3, 4);
 let nameAndAge: [string, number];
 
 nameAndAge = ["Ania", 11];
-nameAndAge[0] = 'Test'
-nameAndAge[1] = 19
+nameAndAge[0] = "Test";
+nameAndAge[1] = 19;
+
+const red: string = "red";
+const green: string = "green";
+const blue: string = "blue";
+
+enum RedGreenBlue {
+	RED = "red",
+	GREEN = "green",
+	BLUE = "blue",
+}
+console.log(RedGreenBlue.GREEN);
 
