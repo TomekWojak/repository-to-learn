3d2dde03a5e807e26c1c9c72fce3788bf841aa3f
Tomek <tomek.wojak796@gmail.com>
2025-09-10 23:51:23 +0200

    practicing changing object keys

--- learning-js-course/new-js-course/script.js
+++ learning-js-course/new-js-course/script.js
@@ -1,6 +1,17 @@
-const arr = [50, 80, 120]
-let sum = 0
+const arr = [50, 80, 120];
+let sum = 0;
 
-for(const el of arr){
-    sum += el
+for (const el of arr) {
+	sum += el;
 }
+
+const carInfo = {
+	brand: "Ford",
+	name: "Mustang",
+	cost: "$40 000",
+	maxSpeed: "300 km/h",
+};
+
+carInfo.name = "Focus";
+
+console.log(carInfo);
