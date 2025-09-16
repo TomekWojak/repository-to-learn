d6a0cfb397d3ec45a04be0a772044a8ab331f111
Tomek <tomek.wojak796@gmail.com>
2025-09-17 00:04:58 +0200

    added function which changing layout of film list

--- css-projects/view-transition/script.js
+++ css-projects/view-transition/script.js
@@ -1,11 +1,14 @@
 const films = document.querySelector(".films");
-
+const filmBtn = document.querySelector("svg.film");
 const animateFilms = () => {
-	const allFilms = films.querySelectorAll("li img");
+	const allFilms = films.querySelectorAll(".rest img");
 	allFilms.forEach((film, index) => {
 		film.style.viewTransitionName = `movie-${index}`;
 	});
-	console.log(allFilms);
+
+	document.startViewTransition(() => {
+		films.classList.add("column");
+	});
 };
 
-animateFilms();
+filmBtn.addEventListener("click", animateFilms);
