b8b00eecdb7a2968a079c318f051ab8caef4b403
Tomek <tomek.wojak796@gmail.com>
2025-09-25 16:26:13 +0200

    set username and picture of random user

--- Js-projects/20RandomUser/script.js
+++ Js-projects/20RandomUser/script.js
@@ -1,11 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-	fetch("https://randomuser.me/api")
-		.then((res) => res.json())
-		.then((data) => dataReady(data));
+	const userInfo = {
+		avatar: document.querySelector(".avatar-img"),
+		userFullName: document.querySelector(".full-name"),
+	};
 
+	async function loadUser() {
+		const response = await fetch("https://randomuser.me/api");
+		const data = await response.json();
+		const prepared = dataReady(data);
+		renderUi(prepared);
+	}
+	const dataReady = (data) => {
+		const result = data.results[0];
+		const fullName = `${result.name.title} ${result.name.first} ${result.name.last}`;
+		const personPictrue = result.picture.large;
 
+		return {
+			fullName,
+			personPictrue,
+		};
+	};
+	const renderUi = ({ fullName, personPictrue }) => {
+		userInfo.avatar.src = personPictrue;
+		userInfo.userFullName.textContent = fullName;
+	};
 
-       const dataReady = (data) => {
-            
-       } 
+	loadUser();
 });
