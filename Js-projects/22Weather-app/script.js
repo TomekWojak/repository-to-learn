6c095d0f05511bc28470bfeaa9bab059c596d9a5
Tomek <tomek.wojak796@gmail.com>
2025-09-26 20:12:38 +0200

    succesfully downloaded data from api

--- Js-projects/22Weather-app/script.js
+++ Js-projects/22Weather-app/script.js
@@ -1,43 +1,69 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const body = document.body;
+	const API_KEY = "46544b0cedb8b4ecd4a5ebc480021ca1";
+	const LOCATION_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
-	navigator.geolocation.getCurrentPosition((position) => {
-		// loadWeatherDataBasedOnUserPosition(position);
+	navigator.geolocation.getCurrentPosition(async (position) => {
+		const userCityInfo = await loadWeatherDataBasedOnUserPosition(position);
+		body.append(renderMainContent(userCityInfo));
 	});
-
-	const renderUI = () => {
-		const container = renderMainContent();
-		body.append(container.searchBox, container.weatherBox);
+	const loadWeatherDataBasedOnUserPosition = async (position) => {
+		const response = await fetch(
+			`${LOCATION_URL}lat=${position.coords.latitude}&lon=${position.coords.longitude}&lang=pl&appid=${API_KEY}&units=metric`
+		);
+		const data = await response.json();
+		return data;
 	};
-	const renderMainContent = () => {
+
+	const renderStaticArea = () => {
 		const searchEngine = createElement("input", ["search-engine"]);
 		const searchButton = createElement("button", ["search-btn"]);
 		const searchBox = createElement("div", ["search-box"]);
-        
+
+		searchButton.textContent = "Sprawdź";
+		searchBox.append(searchEngine, searchButton);
+		body.append(searchBox);
+	};
+	const renderMainContent = ({
+		name,
+		main: { temp, humidity, pressure },
+		wind: { speed },
+		sys: { sunset, sunrise },
+	}) => {
+		const weatherBoxBody = createElement("div", ["body"]);
 		const weatherBox = createElement("div", ["container"]);
 		const weatherCityName = createElement("h2");
 		const weatherStatusImg = createElement("img", ["img"]);
 		const weatherBoxHeader = createElement("div", ["header"]);
-		const weatherBoxBody = createElement("div", ["list"]);
-		const listItems = [];
+		const weatherBoxList = createElement("div", ["list"]);
 
-		searchBox.append(searchEngine, searchButton);
+		weatherCityName.textContent = name;
 
-		for (let i = 0; i <= 7; i++) {
-			const li = createElement("li");
-			const span = createElement("span");
+		weatherBoxList.append(
+			createListItem("Temperatura", temp),
+			createListItem("Wilgotność", humidity),
+			createListItem("Ciśnienie", pressure),
+			createListItem("Szybkość wiatru", speed, "m/s"),
+			createListItem("Wschód słońca", sunrise),
+			createListItem("Zachód słońca", sunset)
+		);
+		weatherBoxHeader.append(weatherStatusImg, weatherCityName);
+		weatherBoxBody.append(weatherBoxList);
+		weatherBox.append(weatherBoxHeader, weatherBoxBody);
 
-			li.append(span);
-
-			listItems.push(li);
-		}
-
-		return {
-			searchBox,
-			weatherBox,
-		};
+		return weatherBox;
 	};
+	const createListItem = (text, stat, optionalUnit) => {
+		const li = createElement("li");
+		const span = createElement("span");
 
+		li.textContent = text;
+		span.textContent = `${stat}${optionalUnit ?? ""}`;
+
+		li.append(span);
+
+		return li;
+	};
 	// helper
 	const createElement = (el, cls = []) => {
 		const element = document.createElement(el);
@@ -46,5 +72,5 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 		return element;
 	};
-	renderUI();
+	renderStaticArea();
 });
