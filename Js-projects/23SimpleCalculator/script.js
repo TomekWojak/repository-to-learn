6fa6eb61a6f0825193561f3bc8477a63e2b4c371
Tomek <tomek.wojak796@gmail.com>
2025-09-29 20:35:18 +0200

    added first function

---
+++ Js-projects/23SimpleCalculator/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const calculator = {
+		buttons: undefined,
+		input: undefined,
+
+        init: function(){
+            this.buttons = document.querySelectorAll('.num')
+        }
+	};
+});
