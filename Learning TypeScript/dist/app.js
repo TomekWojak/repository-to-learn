b2506c743be0479eac42ec3218bd6032de636305
Tomek <tomek.wojak796@gmail.com>
2025-11-26 15:36:27 +0100

    practicing booleans and urlSearchParams

--- Learning TypeScript/dist/app.js
+++ Learning TypeScript/dist/app.js
@@ -1,9 +1,19 @@
 let age = 20; // type inference
 let test; // jeśli puste - trzeba okreslić co to
 // w funkcjach zawsze okreslać typy parametrów
+// union type
 const logAge = (age) => {
     console.log(`Cześć, mam ${age} lat`);
 };
 logAge(20);
 logAge("dwadziescia");
+const button = document.querySelector("button");
+const calculatePrice = (originalPrice, hasDiscount) => {
+    return hasDiscount ? originalPrice * 0.8 : originalPrice;
+};
+button?.addEventListener("click", () => {
+    const originalPrice = 50;
+    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
+    console.log(calculatePrice(originalPrice, hasDiscount));
+});
 export {};
--- Learning TypeScript/src/app.ts
+++ Learning TypeScript/src/app.ts
@@ -3,9 +3,24 @@ let test: number; // jeśli puste - trzeba okreslić co to
 
 // w funkcjach zawsze okreslać typy parametrów
 
+// union type
 const logAge = (age: number | string) => {
 	console.log(`Cześć, mam ${age} lat`);
 };
 
 logAge(20);
 logAge("dwadziescia");
+
+const button = document.querySelector("button");
+
+const calculatePrice = (originalPrice: number, hasDiscount: boolean) => {
+	return hasDiscount ? originalPrice * 0.8 : originalPrice;
+};
+
+button?.addEventListener("click", () => {
+	const originalPrice = 50;
+	const hasDiscount =
+		new URLSearchParams(window.location.search).get("discount") === "true";
+
+	console.log(calculatePrice(originalPrice, hasDiscount));
+});
